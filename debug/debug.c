b9c8441d222d035de75bcd1ec7f6d3dc67cd907a
RagingGrim <armandconnor.duplooy@gmail.com>
2016-10-20 15:26:08 +0200

    Added a few buffer implementations.

--- debug/debug.c
+++ debug/debug.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/errno.h>
+#ifdef __linux__
+	#include <sys/errno.h>
+#endif
 #include "debug.h"
 
+#ifdef __linux__
 void debugMsg( const char *msg , short critical ){
 	printf("Debug %s:%i -> [%s]\n" , __FILE__ , __LINE__ , msg );
 	if( critical )
 		exit(errno);
 }
+#endif
+//TODO: Work on windows error messages.
