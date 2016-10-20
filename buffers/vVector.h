63e56f248c65b0f83751c79ea74360c196a37079
RagingGrim <armandconnor.duplooy@gmail.com>
2016-10-20 16:14:56 +0200

    Added a vector for storing void pointers.

---
+++ buffers/vVector.h
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#ifndef VVECTOR_H
+#define VVECTOR_H
+//defines
+#define VVECTOR_CHUNK_SIZE 1
+
+//structures
+typedef struct vVector{
+size_t _basePointerSize;
+size_t size;
+void **data;
+}vVector , V_VECTOR;
+
+//functions
+vVector *vVector_init();
+void vVector_destroy(vVector *vector);
+short vVector_grow(vVector *vector);
+short vVector_pushback(vVector *vector , void *target);
+void vVector_popback(vVector *vector , short delete);
+void *vVector_at(vVector *vector, size_t n);
+
+
+#endif
