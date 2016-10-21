c67c0552be9756526487f9c5dc44f0d3a38d0acb
grim <armandconnor.duplooy@gmail.com>
2016-10-21 22:37:48 +0200

    Started working on a timer.

--- threads/main.c
+++ threads/main.c
@@ -2,32 +2,17 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-#include "controller.h"
+#include "timer.h"
 #include "../logging/logger.h"
 
-void *l(void *d){
-	logger* log = d;
-	size_t i = 0;
-	while(++i != 15){
-		logIt(log, "IN A THREAD\n");
-	}
+void *say(void *data){
+	puts(data);
 	return NULL;
 }
 
 int main(){
-	logger *log = logger_init(NULL);
-	threadController *tc = threadController_init();
-	logIt(log , "Startup!\n");
-	size_t i = 0;
-	while(++i != 5){
-		pthread_t id;
-		pthread_create(&id, NULL, l, log);
-		threadController_pushback(tc, id);
-
-	}
-	puts("FIN");
-	threadController_stopAll(tc);
-	logger_destroy(log);
-	threadController_destroy(tc);
+	timer Timer;
+	Timer._func = say;
+	Timer._seconds = 50;
 	return 0;
 }
