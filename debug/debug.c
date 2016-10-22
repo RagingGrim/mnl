41273c3690ab71febc5d1e244c0183db5ce3fbfb
grim <armandconnor.duplooy@gmail.com>
2016-10-22 12:01:23 +0200

    Added options to generate raw data.

--- debug/debug.c
+++ debug/debug.c
@@ -12,4 +12,24 @@ void debugMsg( const char *msg , short critical ){
 		exit(errno);
 }
 #endif
+
+char *genData( char Char , size_t Size ){
+	char *buffer = malloc(Size);
+	if( !buffer )
+		return NULL;
+	for( size_t i = 0 ; i < Size ; i++ )
+		buffer[i] = Char;
+	buffer[Size-1] = 0;
+	return buffer;
+}
+
+char *genData_addDelim( char Char , char Delim , size_t Size ){
+	char *buffer = genData( Char , Size + 1 );
+	if( !buffer )
+		return NULL;
+
+	buffer[Size-1] = Delim;
+	buffer[Size] = 0;
+	return buffer;
+}
 //TODO: Work on windows error messages.
--- debug/debug.h
+++ debug/debug.h
@@ -1,5 +1,9 @@
 #ifndef C_DEBUG_H
 #define C_DEBUG_H
+#include <stddef.h>
 void debugMsg( const char *msg , short critical );
 const char *_genDbgMsg( const char *msg );
+
+char *genData( char Char , size_t Size );
+char *genData_addDelim( char Char , char Delim , size_t Size );
 #endif
