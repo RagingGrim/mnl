b78d03cb8ddff064e9ed936fd9c59cf6aa699ef0
RagingGrim <armandconnor.duplooy@gmail.com>
2017-02-25 22:26:59 +0200

    Started preparing to implement threadController and threadInfo objects properly.

--- lib/controller.h
+++ lib/controller.h
@@ -9,34 +9,27 @@
 #include <stdlib.h>
 
 #include "../lib/vVector.h" //<<MARK IGNORE>>
+#include "../lib/circularList.h" //<<MARK IGNORE>>
+
 // DEFINES
 #define THREAD_SIGNAL_READY 0
 
 // STRUCTURES
-typedef struct signalStruct{
-	unsigned short thread_signal;
-	unsigned short master_signal;
-	pthread_mutex_t *_mutex_thread_signal;
-	pthread_mutex_t *_mutex_master_signal;
-}signalStruct, *p_signalStruct;
-
 typedef struct threadController{
 	p_vvector threads;
+	p_vvector threadQueues;
 }threadController , *p_threadController;
+
+typedef struct threadInfo{
+	threadQueue queue;
+	p_vvector reserved;
+}threadInfo, *p_threadInfo;
 
 // FUNCTIONS (THREAD CONTROLLER)
 threadController *threadController_init();
 void threadController_destroy(threadController *tc);
 short threadController_pushback(const threadController *tc,const pthread_t id);
 void threadController_stopAll(const threadController *tc);
-
-// FUNCTIONS (SIGNALS)
-p_signalStruct signal_init();
-void signal_destroy(p_signalStruct signalStruct);
-void signal_set_thread(const p_signalStruct signalStruct,const unsigned short value);
-void signal_set_master(const p_signalStruct signalStruct,const unsigned short value);
-const unsigned short signal_get_thread(const p_signalStruct signalStruct);
-const unsigned short signal_get_master(const p_signalStruct signalStruct);
 
 
 #endif
--- src/controller.c
+++ src/controller.c
@@ -12,8 +12,14 @@ threadController *threadController_init(){
 	if( !tc->threads )
 		goto cleanup_one;
 
+	tc->queues = vvector_init();
+	if( !tc->queues )
+		goto cleanup_two;
+
 	return tc;
 
+	cleanup_two:
+		free(tc->threads);
 	cleanup_one:
 		free(tc);
 	cleanup:
@@ -21,9 +27,13 @@ threadController *threadController_init(){
 }
 
 void threadController_destroy(threadController *tc){
-	for( size_t i = 0 ; i < tc->threads->elements ; i++ )
+	for( size_t i = 0 ; i < tc->threads->elements ; i++ ){
 		free(tc->threads->data[i]);
+		vvector_free(&tc->queues[i]);
+	}
+
 	vvector_free(tc->threads);
+	vvector_free(tc->queues);
 	tc->threads = NULL;
 
 	free(tc);
@@ -32,7 +42,23 @@ void threadController_destroy(threadController *tc){
 
 short threadController_pushback(const threadController *tc,const pthread_t id){
 	pthread_t *temp = malloc(sizeof(pthread_t));
+	if(!temp)
+		return VVECTORE_GROW;
 	*temp = id;
+
+	p_vvector vector = vvector_init();
+	if( !vector ){
+		free(temp);
+		return VVECTORE_GROW;
+	}
+
+	short val = vvector_push(tc->queues, vector);
+	if(val != VVECTORE_GROW){
+		vvector_free(vector);
+		free(temp);
+		return VVECTORE_GROW;
+	}
+
 	return vvector_push(tc->threads , temp);
 }
 
@@ -41,74 +67,4 @@ void threadController_stopAll(const threadController *tc){
 		// pthread_join(*(pthread_t *)vvector_at(tc->threads , i), NULL);
 		pthread_cancel(*(pthread_t *)vvector_at(tc->threads , i));
 	}
-}
-
-
-
-// SIGNALS
-
-p_signalStruct signal_init(){
-	p_signalStruct signalStruct = malloc(sizeof(struct signalStruct));
-	if(!signalStruct)
-		return NULL;
-
-	signalStruct->master_signal = THREAD_SIGNAL_READY;
-	signalStruct->thread_signal = THREAD_SIGNAL_READY;
-	signalStruct->_mutex_thread_signal = malloc(sizeof(pthread_mutex_t));
-	if(!signalStruct->_mutex_thread_signal)
-		goto signal_cleanup_one;
-
-	signalStruct->_mutex_master_signal = malloc(sizeof(pthread_mutex_t));
-	if(!signalStruct->_mutex_thread_signal)
-		goto signal_cleanup_two;
-
-	pthread_mutex_init(signalStruct->_mutex_thread_signal, NULL);
-	pthread_mutex_init(signalStruct->_mutex_master_signal, NULL);
-	return signalStruct;
-
-	signal_cleanup_two:
-	free(signalStruct->_mutex_thread_signal);
-	signal_cleanup_one:
-	free(signalStruct);
-	return NULL;
-}
-
-void signal_destroy(p_signalStruct signalStruct){
-	signalStruct->master_signal = THREAD_SIGNAL_READY;
-	signalStruct->thread_signal = THREAD_SIGNAL_READY;
-
-	pthread_mutex_destroy(signalStruct->_mutex_thread_signal);
-	pthread_mutex_destroy(signalStruct->_mutex_master_signal);
-
-	free(signalStruct->_mutex_thread_signal);
-	free(signalStruct->_mutex_master_signal);
-
-	free(signalStruct);
-	signalStruct = NULL;
-}
-
-void signal_set_thread(const p_signalStruct signalStruct,const unsigned short value){
-	pthread_mutex_lock(signalStruct->_mutex_thread_signal);
-	signalStruct->thread_signal = value;
-	pthread_mutex_unlock(signalStruct->_mutex_thread_signal);
-}
-
-void signal_set_master(const p_signalStruct signalStruct,const unsigned short value){
-	pthread_mutex_lock(signalStruct->_mutex_master_signal);
-	signalStruct->master_signal = value;
-	pthread_mutex_unlock(signalStruct->_mutex_master_signal);
-}
-
-const unsigned short signal_get_thread(const p_signalStruct signalStruct){
-	pthread_mutex_lock(signalStruct->_mutex_thread_signal);
-	unsigned short value = signalStruct->thread_signal;
-	pthread_mutex_unlock(signalStruct->_mutex_thread_signal);
-	return value;
-}
-
-const unsigned short signal_get_master(const p_signalStruct signalStruct){
-	pthread_mutex_lock(signalStruct->_mutex_master_signal);
-	unsigned short value = signalStruct->master_signal;
-	pthread_mutex_unlock(signalStruct->_mutex_master_signal);
-	return value;
 }
