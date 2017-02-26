ffed97de1d8acb9799a9c10a88caff6d5c896204
RagingGrim <armandconnor.duplooy@gmail.com>
2017-02-26 13:11:33 +0200

    Almost finished with generic thread queues.

--- lib/controller.h
+++ lib/controller.h
@@ -15,13 +15,18 @@
 #define THREAD_SIGNAL_READY 0
 
 // STRUCTURES
+typedef struct threadQueue{
+	p_circularList queue;
+	pthread_mutex_t *mutex;
+}threadQueue, *p_threadQueue;
+
 typedef struct threadController{
 	p_vvector threads;
 	p_vvector threadQueues;
 }threadController , *p_threadController;
 
 typedef struct threadInfo{
-	threadQueue queue;
+	p_threadQueue queue;
 	p_vvector reserved;
 }threadInfo, *p_threadInfo;
 
@@ -30,6 +35,19 @@ threadController *threadController_init();
 void threadController_destroy(threadController *tc);
 short threadController_pushback(const threadController *tc,const pthread_t id);
 void threadController_stopAll(const threadController *tc);
+
+// FUNCTIONS (THREAD QUEUES)
+p_threadQueue threadQueue_init();
+void threadQueue_free(p_threadQueue tq);
+void threadQueue_enqueue(const p_threadQueue tq, const void *data);
+void *threadQueue_dequeue(const p_threadQueue tq);
+
+// FUNCTIONS (THREAD INFO)
+p_threadInfo threadInfo_init();
+void threadInfo_free(p_threadInfo ti);
+void threadInfo_enqueue(const p_threadInfo tq, const void *data);
+void *threadInfo_dequeue(const p_threadInfo tq);
+
 
 
 #endif
--- src/controller.c
+++ src/controller.c
@@ -12,14 +12,9 @@ threadController *threadController_init(){
 	if( !tc->threads )
 		goto cleanup_one;
 
-	tc->queues = vvector_init();
-	if( !tc->queues )
-		goto cleanup_two;
 
 	return tc;
 
-	cleanup_two:
-		free(tc->threads);
 	cleanup_one:
 		free(tc);
 	cleanup:
@@ -27,13 +22,11 @@ threadController *threadController_init(){
 }
 
 void threadController_destroy(threadController *tc){
-	for( size_t i = 0 ; i < tc->threads->elements ; i++ ){
+	for( size_t i = 0 ; i < tc->threads->elements ; i++ )
 		free(tc->threads->data[i]);
-		vvector_free(&tc->queues[i]);
-	}
+
 
 	vvector_free(tc->threads);
-	vvector_free(tc->queues);
 	tc->threads = NULL;
 
 	free(tc);
@@ -52,13 +45,6 @@ short threadController_pushback(const threadController *tc,const pthread_t id){
 		return VVECTORE_GROW;
 	}
 
-	short val = vvector_push(tc->queues, vector);
-	if(val != VVECTORE_GROW){
-		vvector_free(vector);
-		free(temp);
-		return VVECTORE_GROW;
-	}
-
 	return vvector_push(tc->threads , temp);
 }
 
@@ -67,4 +53,88 @@ void threadController_stopAll(const threadController *tc){
 		// pthread_join(*(pthread_t *)vvector_at(tc->threads , i), NULL);
 		pthread_cancel(*(pthread_t *)vvector_at(tc->threads , i));
 	}
+}
+
+p_threadQueue threadQueue_init(){
+	p_threadQueue tq = malloc(sizeof(threadQueue));
+	if(!tq)
+		return NULL;
+
+	tq->queue = circularList_init();
+	if(!tq->queue){
+		free(tq);
+		return NULL;
+	}
+
+	tq->mutex = malloc(sizeof(pthread_mutex_t));
+	if(!tq->mutex){
+		free(tq);
+		tq = NULL;
+		return NULL;
+	}
+
+	pthread_mutex_init(tq->mutex, NULL);
+
+	return tq;
+}
+
+// Thread Queue
+void threadQueue_free(p_threadQueue tq){
+	pthread_mutex_destroy(tq->mutex);
+	free(tq->mutex);
+	tq->mutex = NULL;
+	vvector_free(tq->queue);
+	tq->queue = NULL;
+	free(tq);
+	tq = NULL;
+}
+
+void threadQueue_enqueue(const p_threadQueue tq, const void *data){
+	pthread_mutex_lock(tq->mutex);
+	circularList_enqueue(tq->queue, data);
+	pthread_mutex_unlock(tq->mutex);
+}
+
+void *threadQueue_dequeue(const p_threadQueue tq){
+	pthread_mutex_lock(tq->mutex);
+	void *data = circularList_dequeue(tq->queue);
+	pthread_mutex_unlock(tq->mutex);
+	return data;
+}
+
+// Thread Info
+p_threadInfo threadInfo_init(){
+	p_threadInfo ti = malloc(sizeof(threadInfo));
+	if(!ti)
+		return NULL;
+
+	ti->queue = threadQueue_init();
+	if(!ti->queue){
+		free(ti);
+		ti = NULL;
+		return NULL;
+	}
+
+	ti->reserved = vvector_init();
+	if(!ti->reserved){
+		threadQueue_free(ti->queue);
+		free(ti);
+		return NULL;
+	}
+	return ti;
+}
+
+void threadInfo_free(p_threadInfo ti){
+	threadQueue_free(ti->queue);
+	vvector_free(ti->reserved);
+	free(ti);
+	ti = NULL;
+}
+
+void threadInfo_enqueue(const p_threadInfo tq, const void *data){
+	threadQueue_enqueue(tq->queue, data);
+}
+
+void *threadInfo_dequeue(const p_threadInfo tq){
+	return threadQueue_dequeue(tq->queue);
 }
