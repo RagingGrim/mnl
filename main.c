58a92c8994dea64194de92eb5c73d6796eb42a2a
RagingGrim <armandconnor.duplooy@gmail.com>
2017-05-03 16:33:03 +0200

    Removed dummy files.

--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "mnl.h"
-
-int main(){
-	p_threadController tc = threadController_init();
-}
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-extern "C" {
-#include "mnl.h"
-}
-
-int main(const int argc,const char **argv){
-	std::cout << "hurdurhur c++" << std::endl;
-	p_threadController tc = threadController_init();
-}
