be58ceb763480677aea95659d9c7930008345c57
RagingGrim <armandconnor.duplooy@gmail.com>
2017-05-03 18:27:56 +0200

    Minor fix.

--- src/map.c
+++ src/map.c
@@ -60,7 +60,7 @@ basic_map *mapCreate_fromParams(const int argc ,const char **argv){ // TODO: Thi
 	// We want to loop through the command line parameters mapping options.
 	// Any option which does not directly map to state is mapped to (NULL)
 
-	for(size_t i = 1 ; i < argc ; i+=2){
+	for(size_t i = 1 ; i < argc ; i++){
 		if(argv[i][0] == '-'){
 			if(i + 1 < argc){
 				if(argv[i+1][0] != '-'){
